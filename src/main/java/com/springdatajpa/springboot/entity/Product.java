2cbe8fd781c3d7878d9c65aa175249e679795def
cmsjulio <cms.julio1@gmail.com>
2022-03-31 16:54:59 -0300

    20. Using Lombok Library to Reduce Boilerplate Code

--- src/main/java/com/springdatajpa/springboot/entity/Product.java
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -1,5 +1,9 @@
 package com.springdatajpa.springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -7,6 +11,10 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="products",
   schema = "ecommerce", //schema=database;
@@ -17,6 +25,7 @@ import java.time.LocalDateTime;
         )
   })
 public class Product {
+
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_generator")
@@ -97,76 +106,5 @@ public class Product {
   @UpdateTimestamp //insere/atualiza data do último update do atributo automaticamente;
   private LocalDateTime lastUpdated;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getSku() {
-    return sku;
-  }
-
-  public void setSku(String sku) {
-    this.sku = sku;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public BigDecimal getPrice() {
-    return price;
-  }
-
-  public void setPrice(BigDecimal price) {
-    this.price = price;
-  }
-
-  public boolean isActive() {
-    return active;
-  }
-
-  public void setActive(boolean active) {
-    this.active = active;
-  }
-
-  public String getImageUrl() {
-    return imageUrl;
-  }
-
-  public void setImageUrl(String imageUrl) {
-    this.imageUrl = imageUrl;
-  }
-
-  public LocalDateTime getDateCreated() {
-    return dateCreated;
-  }
-
-  public void setDateCreated(LocalDateTime dateCreated) {
-    this.dateCreated = dateCreated;
-  }
-
-  public LocalDateTime getLastUpdated() {
-    return lastUpdated;
-  }
-
-  public void setLastUpdated(LocalDateTime lastUpdated) {
-    this.lastUpdated = lastUpdated;
-  }
 
 }
